b98740b2d37b2e34c3210fba939f18b32d9096c7
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-08-10 15:26:06 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ function Navbar() {
         <div className="md:flex items-center">
         <div className=" flex items-center gap-4 text-black  ml-5">
         <a
-                  href="https://x.com/SnowPepe_Sol" rel="noreferrer" target="_blank"
+                  href="https://x.com/" rel="noreferrer" target="_blank"
                   className=" text-xl text-white bg-[#05a3c9] hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000000]  hover:text-[#fff]"
                 >
                   <BsTwitterX />
@@ -41,7 +41,7 @@ function Navbar() {
            
 
                 <a
-                  href="https://t.me/snowpepe_sol" target="_blank"  rel="noreferrer"
+                  href="https://t.me/" target="_blank"  rel="noreferrer"
                   className=" text-xl text-white bg-[#05a3c9] hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000000]  hover:text-[#fff]"
                 >
                   <FaTelegram />
@@ -95,7 +95,7 @@ function Navbar() {
             
             <div className=" flex items-center gap-4 text-black mb-6 ml-5">
             <a
-                  href="https://x.com/SnowPepe_Sol"
+                  href="https://x.com/"
                   className=" text-xl text-black bg-[#05a3c9] hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000]  hover:text-[#fff]"
                 >
                   <BsTwitterX />
@@ -104,7 +104,7 @@ function Navbar() {
            
 
                 <a
-                  href="https://t.me/snowpepe_sol" target="_blank" rel="noreferrer"
+                  href="https://t.me/" target="_blank" rel="noreferrer"
                   className=" text-xl text-black bg-[#05a3c9] hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000]  hover:text-[#fff]"
                 >
                   <FaTelegram />
